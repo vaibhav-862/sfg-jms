979b33514581f839749dfca65eee64ec67f7d79e
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-25 19:18:47 +0530

    adding send JMS message example. Closes #6

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -12,10 +12,13 @@ public class SfgJmsApplication {
     public static void main(String[] args) throws Exception {
 
         //setting embedded server
+        //set security enabled property to 'false' to avoid below exception on latest SpringBoot version
+        //Caused by: javax.jms.JMSSecurityException: AMQ229031: Unable to validate user
         ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
         .setPersistenceEnabled(false)
         .setJournalDirectory("target/data/journal")
-        .addAcceptorConfiguration("invm", "vm://0"));
+        .addAcceptorConfiguration("invm", "vm://0")
+        .setSecurityEnabled(false));
 
         server.start();
 
